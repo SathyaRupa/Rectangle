37aa4a9a772ababe661377ba3d80e259abfa38e5
Sathya S <sathyarupa2002@gmail.com>
2024-01-04 05:37:37 +0000

    Refactor. changed the datatype from int to double

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -2,19 +2,27 @@ package com.thoughtworks.rectangle;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
 
 import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
     @Test
     void shouldReturnAreaOf24WhenTheLengthIs6AndBreadthIs4(){
-        int length = 6;
-        int breadth = 4;
-        int expected = 24;
 
-        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle1 = new Rectangle(6,4);
+        Rectangle rectangle2 = new Rectangle(5.1,7.5);
 
-        assertThat(rectangle.area(length,breadth), equalTo(expected));
+        double expected1 = 24;
+        double expected2 = 38.25;
+        
+        double actualArea1 = rectangle1.area();
+        double actualArea2 = rectangle2.area();
+
+        assertThat(actualArea1, is(closeTo(expected1,0.001)));
+        assertThat(actualArea2, is(closeTo(expected2,0.001)));
 
     }
+
 }
